a4ea928e62db6b3bd3f711cdbfbd3d5cc0ea77af
Sherdooh <Sherkiplah@gmail.com>
2024-09-05 15:16:41 +0300

    display message upon adding data

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 import express from "express";
 
+
 const app = express();
 const port = 8080;
 
+app.use(express.json())
+
 app.get('/', (req,res) => {
+
+    
     res.send('This is Node');
+})
+
+app.post('/add', (req, res) => {
+    const data = []
+    data.push(req.body);  
+
+    res.status(201).json({
+        message: "Data added successfully",
+        data : data,
+    });
+
 })
 
 app.listen(port, () => {

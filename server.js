beee0524ecaea32216b2e4bcbbbadb64d13d3429
Sherdooh <Sherkiplah@gmail.com>
2024-09-05 13:27:09 +0300

    express server update

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req,res) => {
+    res.send('This is Node');
+})
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`);
+})
+
+
